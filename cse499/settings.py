c21e10854aa068b246cd07d1a91e1753b8099887
Daiyan Rafi <daiyanrafi1@gmail.com>
2022-06-26 23:55:18 +0600

    first

--- cse499/settings.py
+++ cse499/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3%y3laftm62q0zaj+s7#p-xqq9(&#q+)s8)p-&#&bz*0$!xu$0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1' , 'laundrylagbe499.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
